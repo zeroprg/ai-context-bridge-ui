796fc2a289ee22cf11f91224fb5c9a9d5d8acc49
Vitaly Arkhipov <zeroprg@yahoo.com>
2023-12-15 17:00:30 -0700

    f

--- docker-run.sh
+++ docker-run.sh
@@ -2,5 +2,6 @@ docker run -d \
   -v /etc/letsencrypt/live/tothemoon.chat/fullchain.pem:/etc/nginx/ssl/fullchain.pem \
   -v /etc/letsencrypt/live/tothemoon.chat/privkey.pem:/etc/nginx/ssl/privkey.pem \
   -p 80:80 -p 443:443 \
-  --name your-nginx-container \
+  --restart=always \
+  --name ai-context-bridge-ui \
   zeroprg/ai-context-bridge-ui
